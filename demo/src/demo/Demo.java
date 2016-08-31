9c754af2acc57df9715267ed36c1f236fe8e7a63
Merxvell <Merxvell@gmail.com>
2016-08-31 16:11:07 -0400

    demo finished for class

--- demo/src/demo/Demo.java
+++ demo/src/demo/Demo.java
@@ -18,7 +18,7 @@ public class Demo {
         // TODO code application logic here
         for (int i = 1; i <= 10; i++) {
             if (i == 10) {
-                System.out.println("the recursive method is over");
+                System.out.println("the method is over");
                 double a = Math.log(i) / Math.log(2);
                 System.out.println(a);
                 double b = a * a;
@@ -29,7 +29,7 @@ public class Demo {
                 System.out.println(ai + "\t" + bi + "\t" + ci + "\t" + i * i + "\t" + i * i * i + "\t\t" + di);
                 System.out.println("\n");
             } else {
-                System.out.println("the recursive method is on " + i);
+                System.out.println("the method is on " + i);
                 double a = Math.log(i) / Math.log(2);
                 System.out.println(a);
                 double b = a * a;
